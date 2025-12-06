b0065291859b45f4cf378eb9bb79639a5187f4c9
mrvgcytr <105017460+mrvgcytr@users.noreply.github.com>
2025-12-06 11:24:17 +0100

    Create K2_K4_Customer.sql
    add file with some new exercises

---
+++ VL_Beispiele/Klausurvorbereitung.sql
@@ -0,0 +1,42 @@
+-- 1. SQL Queries
+-- Basis: Beispieldatenbank OEHR
+
+-- 1.1  Basics & Subqueries 
+
+-- 1.1.1 Ermitteln Sie Vor- und Nachnamen der Kunden aus der Tabelle "OEHR_CUSTOMERS"
+
+
+
+
+-- 1.1.2 Ermitteln Sie PLZ, Vor- und Nachnamen der Kunden aus der Tabelle "OEHR_CUSTOMERS", sortiert nach absteigender PLZ
+
+
+
+
+-- 1.1.3 In der Tabelle OEHR_CUSTOMERS ist das Kreditlimit pro Kunden hinterlegt. Ermitteln Sie den durchschnittlichen Wert für "CREDIT_LIMIT" der Kunden 
+
+
+
+
+-- 1.1.4 Das Management überlegt sich das Kreditlimit um 30% zu erhöhen. Hiervon sollen aber nur Kundne ptrofitieren, die weniger als den Durchschnittswert verfügen. Bestimmen Sie den Vor- & Nachnamen, das bisherige Kreditlimit und das erhöhte Kreditlimit der betroffenen Kunden an. 
+
+
+
+
+
+
+-- 1.1.5 Gruppieren Sie gleiche Kreditlimits und geben Sie die Anzahl an Kunden für jede Gruppe an.     
+
+
+
+
+
+-- 1.1.6 Gib pro "STATE_PROVINCE" die Anzahl an Kunden an und sortiere nach absteigender Kundenanzahl 
+
+
+
+
+
+-- 1.1.7 Gib pro "STATE_PROVINCE" die Anzahl an Kunden an und sortiere nach absteigender Kundenanzahl 
+
+
